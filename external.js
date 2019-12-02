f3f7ec89a64cd2b06b10ad1bd5f66aa3fcf9460d
Yang <t_y24@txstate.edu>
2019-12-01 21:59:33 -0600

    Add buffer div.Fixed style.

--- external.js
+++ external.js
@@ -8,11 +8,12 @@
       "esri/widgets/BasemapToggle",
       "esri/widgets/Fullscreen",
       "esri/widgets/Search",
+      "esri/widgets/TimeSlider",
+      "esri/widgets/Legend",
       "esri/widgets/LayerList",
       "esri/layers/GraphicsLayer",
       "esri/tasks/QueryTask",
       "esri/tasks/support/Query",
-      "esri/widgets/TimeSlider",
       "esri/layers/support/Field",
       "esri/intl",
       "esri/popup/support/FieldInfoFormat",
@@ -21,34 +22,117 @@
       "dojo/on",
       "dojo/domReady!"
     ],
-function(Map, MapView, TileLayer, MapImageLayer, FeatureLayer, BasemapToggle, Fullscreen, Search, LayerList, GraphicsLayer, QueryTask, Query, TimeSlider, Field, intl, FieldInfoFormat, arrayUtils, dom, on){
-  // Create a feature layer displaying data
+function(Map, MapView, TileLayer, MapImageLayer, FeatureLayer, BasemapToggle, Fullscreen, Search, TimeSlider, Legend, LayerList, GraphicsLayer, QueryTask, Query, Field, intl, FieldInfoFormat, arrayUtils, dom, on){
+  // Create feature layers displaying data of ITC fire and air monitoring
   var MonitoringLayer = new FeatureLayer({
     url: "https://services5.arcgis.com/w5EzInDqtu24PScU/arcgis/rest/services/ITC_Air_Monitoring_Map(BZ)/FeatureServer/0",
     outFields: ["DateTime","Type","VOC","Benzene","Xylene","Toluene", "Naptha", "H2S", "CO", "PM", "Agency", "Detected"],
     popupTemplate: popupTemplate,
   });
+  var ITCTanksLayer = new FeatureLayer({
+    url: "https://services5.arcgis.com/w5EzInDqtu24PScU/arcgis/rest/services/ITC_Fire_Map/FeatureServer/0",
+    outFields: ["Name","SymbolID","PopupInfo","TankConten"],
+    popupTemplate: iTCTanksTemplate,
+  });
+  var ITCFireBoundaryLayer = new FeatureLayer({
+    url: "https://services5.arcgis.com/w5EzInDqtu24PScU/arcgis/rest/services/ITC_Fire_Map/FeatureServer/1",
+    outFields: ["Name","Shape_Area"],
+    popupTemplate: iTCBoundaryTemplate,
+  });
+
+  var ITCBoundaryLayer = new FeatureLayer({
+    url: "https://services5.arcgis.com/w5EzInDqtu24PScU/arcgis/rest/services/ITC_Fire_Map/FeatureServer/2",
+    outFields: ["Name","Shape_Area"],
+    popupTemplate: iTCBoundaryTemplate,
+  });
+
+  // Create popup templates for ITC fire related features
+  var iTCTanksTemplate = {
+    title: "{Name}",
+    content: "<b>Tank content:</b> {TankConten}<br>" +
+             "<b>Description:</b> {PopupInfo}<br>",
+    fieldInfos: [
+      {
+        fieldName: "Name", // Set field name to display in popups
+        label: "Name",     // Set field alias
+      },{
+        fieldName: "TankConten", // Set field name to display in popups
+        label: "TankConten",     // Set field alias
+      },{
+        fieldName: "PopupInfo", // Set field name to display in popups
+        label: "PopupInfo",     // Set field alias
+      },
+    ]
+  };
+  var iTCBoundaryTemplate = {
+    title: "{Name}",
+    content: "<b>Area:</b> {Shape_Area}<br>",
+    fieldInfos: [
+      {
+        fieldName: "Name", // Set field name to display in popups
+        label: "Name",     // Set field alias
+      },{
+        fieldName: "Shape_Area", // Set field name to display in popups
+        label: "Shape_Area",     // Set field alias
+      },
+    ]
+  };
 
   // Create a tile layer by referencing its URL
   var WorldMapaLayer = new TileLayer({
-    url: "https://server.arcgisonline.com/arcgis/rest/services/Reference/World_Boundaries_and_Places/MapServer"
+    url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer"
   });
+  /*
+  // Create HeatMap, set heatmap renderer
+  var heatRenderer = {
+    type: "heatmap",
+    colorStops: [
+      { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+      { color: "#472b77", ratio: 0.083 },
+      { color: "#4e2d87", ratio: 0.166 },
+      { color: "#563098", ratio: 0.249 },
+      { color: "#5d32a8", ratio: 0.332 },
+      { color: "#6735be", ratio: 0.415 },
+      { color: "#7139d4", ratio: 0.498 },
+      { color: "#7b3ce9", ratio: 0.581 },
+      { color: "#853fff", ratio: 0.664 },
+      { color: "#a46fbf", ratio: 0.747 },
+      { color: "#c29f80", ratio: 0.83 },
+      { color: "#e0cf40", ratio: 0.913 },
+      { color: "#ffff00", ratio: 1 }
+    ],
+    maxPixelIntensity: 25,
+    minPixelIntensity: 0
+  };
 
+  var HeatMapLayer = new GraphicsLayer({
+    url: "https://services5.arcgis.com/w5EzInDqtu24PScU/arcgis/rest/services/ITC_Air_Monitoring_Map(BZ)/FeatureServer/0",
+    popupTemplate: popupTemplate,
+    renderer: heatRenderer
+  });
+  */
   // Create a map then set its basemap and tilelayers.
   var map = new Map({
     basemap: "topo-vector",
-    layers: [MonitoringLayer , WorldMapaLayer]
+    layers: [WorldMapaLayer, ITCBoundaryLayer, ITCFireBoundaryLayer,ITCTanksLayer, MonitoringLayer]
   });
 
   // Set a mapview
   var view = new MapView({
     container: "viewDiv",           // Put the mapview in a division (element id="viewDiv")
     map: map,                       // Asign the predefined map to the mapview
-    zoom: 10,                       // Set zoom level
+    zoom: 12,                       // Set zoom level
     center: [-95.096093, 29.734190]    // Set mapview center point
   });
 
-
+  /*// Add heat map legent to view
+  view.ui.add(
+    new Legend({
+      view: view
+    }),
+    "bottom-left"
+    );
+  */
   // Set on-hover feature pop-up info window
   view.on("click", function(event){
     view.popup.open({
@@ -109,6 +193,30 @@ function(Map, MapView, TileLayer, MapImageLayer, FeatureLayer, BasemapToggle, Fu
     view.ui.add("optionsDiv", "bottom-right"); // Add the new division for query to the bottom-right corner of the mapview,
     on(dom.byId("doBtn"), "click", doQuery);   // Set to script to run callbak function "doQuery" on click of the query bottom.
   });
+  /*
+  // Set buffer tool
+  on(dom.byId("buffer"), 'click', function(evt){
+    var params = new BufferParameters();
+    params.distances = [10];
+    params.bufferSpatialReference = map.spatialReference;
+    params.outSpatialReference = map.spatialReference;
+    params.unit = GeometryService.UNIT_STATUTE_MILE
+    console.log(graphic);
+    params.geometries = [graphic.geometry];
+    console.log(params);
+    gs.buffer(params, showBuffer);
+  })
+
+  function showBuffer(bufferedGeometries) {
+    console.log(bufferedGeometries);
+    var symbol = new SimpleFillSymbol("none", new SimpleLineSymbol("dashdot", new Color([255, 0, 0]), 2), new Color([255, 255, 0, 0.25]));
+
+    array.forEach(bufferedGeometries, function (geometry) {
+        graphic = new Graphic(geometry, symbol);
+        map.graphics.add(graphic);
+    });
+  }
+*/
 /*
   // Set hover-on pop-up displaying feature FieldInfoFormatview.on("click", function(event){
   view.on("hover", function(event){
